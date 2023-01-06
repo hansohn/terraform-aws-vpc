9d809583e5778cec0f353b8461ba197853f9fe5b
Ryan Hansohn <hansohn@users.noreply.github.com>
2023-01-05 20:26:54 -0800

    fix index refferences

--- modules/route/outputs.tf
+++ modules/route/outputs.tf
@@ -3,6 +3,6 @@
 #--------------------------------------------------------------
 
 output "id" {
-  value       = element(concat(aws_route.this.*.id, [""]), 0)
+  value       = element(concat(aws_route.this[*].id, [""]), 0)
   description = "Route Table identifier and destination"
 }
